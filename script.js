8186677e0cb223bc2e826a3670ff0a0e849bd0ba
Linas Mintautiškis <linas.mintautiskis@gmail.com>
2022-12-14 19:08:46 +0200

    Finished functionality and simple UI

--- script.js
+++ script.js
@@ -1,34 +1,197 @@
-const Web3 = require("web3");
-const web3 = new Web3("http://127.0.0.1:7545/");
-const lotteryJSON = require("./build/contracts/Lottery.json");
-
-// const getAccounts = async () => {
-//     try {
-//       const accounts = await web3.eth.getAccounts();
-//       console.log(accounts);
-//     } catch (error) {
-//       console.log("ACCOUNTS ERROR", error);
-//     }
-// }
-
-// getAccounts();
-
-const contract_address = "0x08D4098AB7599d980F0d25Fbf334056Cf98A46cC";
-
-const lottery = new web3.eth.Contract(lotteryJSON.abi, contract_address);
-
-const getPlayers = async () => {
-    try {
-      const response = await lottery.methods.getPlayers().call();
-      console.log(response);
-    } catch (error) {
-      console.log("ERROR GETTING PLAYERS")
+let account;
+const connectMetamask = async () =>{
+    if (window.ethereum !== "undefined") {
+    const accounts = await ethereum.request({method: "eth_requestAccounts"});
+    account = accounts[0];
+    document.getElementById("accountArea").innerHTML = account;
     }
 }
 
-getPlayers();
+const connectContract = async () => {
+    const ABI = [
+{
+"inputs": [],
+"name": "enter",
+"outputs": [],
+"stateMutability": "payable",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "pickWinner",
+"outputs": [],
+"stateMutability": "nonpayable",
+"type": "function"
+},
+{
+"inputs": [],
+"stateMutability": "nonpayable",
+"type": "constructor"
+},
+{
+"inputs": [],
+"name": "getBalance",
+"outputs": [
+    {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "getPlayers",
+"outputs": [
+    {
+        "internalType": "address payable[]",
+        "name": "",
+        "type": "address[]"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "getRandomNumber",
+"outputs": [
+    {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [
+    {
+        "internalType": "uint256",
+        "name": "lottery",
+        "type": "uint256"
+    }
+],
+"name": "getWinnerByLottery",
+"outputs": [
+    {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [
+    {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }
+],
+"name": "lotteryHistory",
+"outputs": [
+    {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "lotteryId",
+"outputs": [
+    {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "owner",
+"outputs": [
+    {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+},
+{
+"inputs": [
+    {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }
+],
+"name": "players",
+"outputs": [
+    {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+}
+];
+    const Address = "0x08D4098AB7599d980F0d25Fbf334056Cf98A46cC";
+    window.web3 = await new Web3(window.ethereum);
+    window.contract = await new window.web3.eth.Contract( ABI, Address); 
+    document.getElementById("contractArea").innerHTML = "connected to smart contract";
+}
 
-// const betInfo = await RouletteWeb.methods.getPlayerLastGameRound().call({
-//     from: addr,
-//     value: '0'
-//   });
+const enter = async () => {
+try {
+    await window.contract.methods.enter().send({
+        from: account,
+        value: '1000000000000000000',
+        gas: 3000000,
+        });
+    let balance = await window.contract.methods.getBalance().call();
+    document.getElementById("prizePool").innerHTML = balance/1000000000000000000;
+}
+catch (error) {
+    console.log("ERROR ENTERING", error);
+}
+}
+
+// const refresh = async () => {
+//     try {
+//         let balance = await window.contract.methods.getBalance().call();
+//         document.getElementById("prizePool").innerHTML = balance/1000000000000000000;
+//     }
+//     catch (error) {
+//         console.log("ERROR ENTERING", error);
+//     }
+// }
+
+const pickWinner = async () => {
+    try {
+        await window.contract.methods.pickWinner().send({ 
+            from: account,
+        });
+        const lottID = await window.contract.methods.lotteryId().call();
+        const winner = await window.contract.methods.getWinnerByLottery(lottID-1).call();
+        document.getElementById("winner").innerHTML = "Winner is " + winner;
+    }
+    catch (error) {
+        console.log("ERROR PICKING", error);
+    }
+}
